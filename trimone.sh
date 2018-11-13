fcf48b1f716187a3f9dfdd2cba703a3222036fb5
Ozan Kiratli <ozankiratli@protonmail.com>
2018-11-13 16:01:24 -0500

    sudo commands removed

--- trimone.sh
+++ trimone.sh
@@ -8,10 +8,10 @@ DATADIR=$WD/Data
 INPUT1=$1
 INPUT2=$2
 
-sudo $TRIMGALORE $TRIMGALOREOPTIONS $INPUT1 $INPUT2
+$TRIMGALORE $TRIMGALOREOPTIONS $INPUT1 $INPUT2
 wait
-sudo mv *.zip reports/fastqc
-sudo mv *.html reports/fastqc
-sudo mv *.txt reports/trim
-sudo mv *_unpaired_* unpaired/
-sudo mv *val* trimmed/
+mv *.zip reports/fastqc
+mv *.html reports/fastqc
+mv *.txt reports/trim
+mv *_unpaired_* unpaired/
+mv *val* trimmed/
