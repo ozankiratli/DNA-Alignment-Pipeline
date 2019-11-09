36eacb106a356adb102130a29a9ef45a5a3f16eb
evolozzy <ozankiratli@protonmail.com>
2019-11-09 09:33:59 -0500

    Fixed checkinstalled for picard-tools/picard.jar, now checks picard-tools first, then checks if picard.jar is there.

--- mtDNA/checkinstalled.sh
+++ mtDNA/checkinstalled.sh
@@ -10,8 +10,7 @@ rm -f $tmpfile
 touch $tmpfile
 
 JAVP=`$JAVA -version 2>&1`
-if [ -z "$JAVP" ]
-then
+if [ -z "$JAVP" ] ; then
 	echo "java is not installed or wrong path in PROGRAMPATHS file." >> $tmpfile
 else
 	echo "java is installed" >> $tmpfile
@@ -19,18 +18,21 @@ fi
 rm -f 1
 wait
 
-PICNP=`ls $PICARDP`
-if [ -z "$PICNP" ]
-then
-	echo "picard.jar is not installed or wrong path in PROGRAMPATHS file." >> $tmpfile
+PICNP1=`$PICARD ViewSam --version 2>&1`
+if [ -z "$PICNP1" ] ; then
+	PICNP2=`ls $PICARDP`
+	if [ -z "$PICNP2" ] ; then
+		echo "picard is not installed or wrong path in PROGRAMPATHS file." >> $tmpfile
+	else
+		echo "picard.jar is installed" >> $tmpfile
+	fi
 else
-	echo "picard.jar is installed" >> $tmpfile
+	echo "picard-tools is installed" >> $tmpfile
 fi
 wait
 
 SAMP=`$SAMTOOLS --version`
-if [ -z "$SAMP" ]
-then
+if [ -z "$SAMP" ] ; then
 	echo "samtools is not installed or wrong path in PROGRAMPATHS file." >> $tmpfile
 else
 	echo "samtools is installed" >> $tmpfile
@@ -38,8 +40,7 @@ fi
 wait
 
 BAMP=`$BAMTOOLS --version`
-if [ -z "$BAMP" ]
-then
+if [ -z "$BAMP" ] ; then
 	echo "bamtools is not installed or wrong path in PROGRAMPATHS file." >> $tmpfile
 else
 	echo "bamtools is installed" >> $tmpfile
@@ -47,8 +48,7 @@ fi
 wait
 
 VUTP=`ls $VCFUTILS`
-if [ -z "$VUTP" ]
-then
+if [ -z "$VUTP" ] ; then
 	echo "vcfutils.pl is not installed or wrong path in PROGRAMPATHS file." >> $tmpfile
 else
 	echo "vcfutils.pl is installed" >> $tmpfile
@@ -56,8 +56,7 @@ fi
 wait
 
 BCFP=`$BCFTOOLS -v`
-if [ -z "$BCFP" ]
-then
+if [ -z "$BCFP" ] ; then
 	echo "bcfools is not installed or wrong path in PROGRAMPATHS file." >> $tmpfile
 else
 	echo "bcftools is installed" >> $tmpfile
@@ -65,8 +64,7 @@ fi
 wait
 
 SQTP=`$SEQTK 2>&1 | grep "Version"`
-if [ -z "$SQTP" ]
-then
+if [ -z "$SQTP" ] ; then
 	echo "seqtk is not installed or wrong path in PROGRAMPATHS file." >> $tmpfile
 else
 	echo "seqtk is installed" >> $tmpfile
@@ -74,8 +72,7 @@ fi
 wait
 
 BWAP=`$BWA 2>&1 | grep "Version"`
-if [ -z "$BWAP" ]
-then
+if [ -z "$BWAP" ] ; then
 	echo "bwa is not installed or wrong path in PROGRAMPATHS file." >> $tmpfile
 else
 	echo "bwa is installed" >> $tmpfile
@@ -83,8 +80,7 @@ fi
 wait
 
 TGLP=`$TRIMGALORE --version`
-if [ -z "$TGLP" ]
-then
+if [ -z "$TGLP" ] ; then
 	echo "trim_galore is not installed or wrong path in PROGRAMPATHS file." >> $tmpfile
 else
 	echo "trim_galore is installed" >> $tmpfile
@@ -92,8 +88,7 @@ fi
 wait
 
 FBYP=`$FREEBAYES --version`
-if [ -z "$FBYP" ]
-then
+if [ -z "$FBYP" ] ; then
 	echo "freebayes is not installed or wrong path in PROGRAMPATHS file." >> $tmpfile
 else
 	echo "freebayes is installed" >> $tmpfile
@@ -101,8 +96,7 @@ fi
 wait
 
 CTAP=`$CUTADAPT --version`
-if [ -z "$CTAP" ]
-then
+if [ -z "$CTAP" ] ; then
 	echo "cutadapt is not installed or wrong path in PROGRAMPATHS file." >> $tmpfile
 else
 	echo "cutadapt is installed" >> $tmpfile
@@ -111,8 +105,7 @@ wait
 
 FASTQCPATH=`echo $FASTQC | sed 's_/fastqc__g'`
 CHECKFQCPATH=`echo $PATH | grep $FASTQCPATH`
-if [ -z "$CHECKFQCPATH" ]
-then
+if [ -z "$CHECKFQCPATH" ] ; then
 	PATH=$PATH:$FASTQCPATH
 	export PATH=$PATH
 	echo "FastQC path is added to the path"
@@ -122,8 +115,7 @@ fi
 wait
 
 FQCP=`fastqc --version`
-if [ ! -z "$FQCP" ]
-then
+if [ ! -z "$FQCP" ] ; then
 	echo "fastqc is installed" >> $tmpfile
 else
 	echo "fastqc is not installed or wrong path in PROGRAMPATHS file." >> $tmpfile
